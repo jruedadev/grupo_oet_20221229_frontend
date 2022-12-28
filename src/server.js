290866bd7c42bc18c1ce9b0ffeeb7a403d3813eb
Johan Rueda <librajohan2000@gmail.com>
2022-12-28 11:26:19 -0500

    Ajuste server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/frontend'));
+console.log(__dirname + '/../dist/frontend');
+app.use(express.static(__dirname + '/../dist/frontend'));
 // app.get('/', function (req, res) {
 //     res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 // });
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/dist/frontend/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname + '/../dist/frontend/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(process.env.PORT || 8080);
