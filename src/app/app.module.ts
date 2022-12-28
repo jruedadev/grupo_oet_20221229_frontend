fd358e8c46b8d4d76b116375d14c0c57501716b2
Johan Rueda <librajohan2000@gmail.com>
2022-12-28 13:55:50 -0500

    Componentes registrados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { VehiclesComponent } from './app/vehicles/vehicles.component';
+import { OwnersComponent } from './app/owners/owners.component';
+import { DriversComponent } from './app/drivers/drivers.component';
+import { ReportsComponent } from './app/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VehiclesComponent,
+    OwnersComponent,
+    DriversComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
